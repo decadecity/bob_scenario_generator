c441c8e68cf2ed21c6d0aecd32b984a133046795
Orde Saunders <orde@decadecity.net>
2017-08-14 21:56:01 +0100

    Addds the types to the mix.

--- app/generator.js
+++ app/generator.js
@@ -5,14 +5,33 @@ module.exports.get_playable = function(aircraft) {
 };
 
 module.exports.get_periods = function(aircraft) {
+	const periods_avail = ['early', 'mid', 'late', 'blitz'];
 	return _.uniq(
 		_.reduce(aircraft, function(periods, a) {
-			['early', 'mid', 'late', 'blitz'].forEach(function(period) {
+			periods_avail.forEach(function(period) {
 				if (a[period]) {
 					periods.push(period);
 				}
 			});
 			return periods;
+		}, [])
+	);
+};
+
+module.exports.get_types = function(aircraft, periods) {
+	const types_avail = ['common', 'outlandish'];
+	return _.uniq(
+		_.reduce(aircraft, function(types, a) {
+			periods.forEach(function(period) {
+				if (a[period]) {
+					types_avail.forEach(function(type) {
+						if (a[type]) {
+							types.push(type);
+						};
+					});
+				}
+			});
+			return types;
 		}, [])
 	);
 };
--- test/generator.js
+++ test/generator.js
@@ -8,22 +8,33 @@ const aircraft = require('../app/aircraft');
 const available_aircraft = [
   aircraft.new(1, true, 'Hurricane', 3, 2, 2, 0, 3, 1, 3, 1, 2, [11, 12]),
   aircraft.new(2, true, 'Spitfire', 3, 3, 3, 3, 3, 1, 3, 2, 0, [11, 12]),
-  aircraft.new(2, true, 'Defiant', 3, 1, 0, 0, 2, 1, 3, 1, 3, 2, [11, 12]),
+  aircraft.new(3, true, 'Defiant', 3, 1, 0, 0, 2, 1, 3, 1, 3, 2, [11, 12]),
+  aircraft.new(5, true, 'Gladiator', 1, 0, 0, 0, 0, 3, 1, 0, 2, [11, 12]),
   aircraft.new(11, false, 'He 111', 3, 3, 3, 3, 3, 1, 3, 2, 0),
 ];
 
 describe("Generator", function() {
 	let playable;
+	let periods;
 
 	it("should return a list of playable aircraft", function() {
 		playable = generator.get_playable(available_aircraft);
-		assert.deepEqual(playable, [available_aircraft[0], available_aircraft[1], available_aircraft[2]]);
+		assert.deepEqual(playable, [
+			available_aircraft[0],
+			available_aircraft[1],
+			available_aircraft[2],
+			available_aircraft[3],
+		]);
 	});
 
 	it("should list the available periods", function() {
-		assert.deepEqual(generator.get_periods(playable), ['early', 'mid', 'late', 'blitz']);
+		periods = ['early', 'mid', 'late', 'blitz'];
+		assert.deepEqual(generator.get_periods(playable), periods);
 	});
 
-
+	it("should list the available types", function() {
+		assert.deepEqual(generator.get_types([available_aircraft[0]], ['early']), ['common', 'outlandish']);
+		assert.deepEqual(generator.get_types([available_aircraft[3]], ['early']), ['outlandish']);
+	});
 
 });
